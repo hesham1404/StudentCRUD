e29a17dc2e39f6c907c8fcc11fae05da31338f5c
Hesham MATT <matthesham@gmail.com>
2026-09-04 18:59:13 +0530

    welcome

--- app/login/page.js
+++ app/login/page.js
@@ -46,7 +46,7 @@ export default function Login() {
 
           <div className="text-center mb-8">
             <h1 className="text-3xl font-bold text-white">
-              Welcome Back
+              Welcome
             </h1>
 
             <p className="text-slate-400 mt-2 text-sm">
